00e93404a45b04f11519c1d8eaebbab878de3335
Abbie <abigail@ient.me>
2024-12-14 16:33:56 +0000

    add some basics

--- src/main/java/cc/abbie/amap/AMap.java
+++ src/main/java/cc/abbie/amap/AMap.java
@@ -1,10 +1,20 @@
 package cc.abbie.amap;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AMap implements ModInitializer {
 
+    public static final String MODID = "amap";
+    public static final Logger LOGGER = LoggerFactory.getLogger("AMap");
+
     @Override
     public void onInitialize() {
+    }
+
+    public static ResourceLocation id(String path) {
+        return new ResourceLocation(MODID, path);
     }
 }
