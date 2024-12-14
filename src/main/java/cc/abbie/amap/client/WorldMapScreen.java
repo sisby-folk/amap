1eb01d339ceaad111359744ba586c9acb6b04f5e
Abbie <abigail@ient.me>
2024-12-14 22:33:52 +0000

    render bg in world map screen

--- src/main/java/cc/abbie/amap/client/WorldMapScreen.java
+++ src/main/java/cc/abbie/amap/client/WorldMapScreen.java
@@ -1,10 +1,17 @@
 package cc.abbie.amap.client;
 
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class WorldMapScreen extends Screen {
-    protected WorldMapScreen() {
+    public WorldMapScreen() {
         super(Component.translatable("screen.amap.world_map"));
+    }
+
+    @Override
+    public void render(GuiGraphics gui, int mouseX, int mouseY, float partialTick) {
+        renderBackground(gui);
+        super.render(gui, mouseX, mouseY, partialTick);
     }
 }
